b7566b87e36269d9d9407cef77a8fad53134ed32
yoohan-dex <yao460733480@outlook.com>
2016-05-20 16:34:25 +0800

    require postcss

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,25 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
   notify = require('gulp-notify'),
    bower = require('gulp-bower');
-  
-var postcss       = require('gulp-postcss');
 
-var autoprefixer  = require('autoprefixer');  
-var scss          = require('postcss-scss');
-var cssnext       = require('postcss-cssnext');
-var nested        = require('postcss-nested');
-var browserSync   = require('browser-sync').create();
-var reload        = browserSync.reload;
+var postcss = require('gulp-postcss');
+
+var autoprefixer = require('autoprefixer');
+var scss = require('postcss-scss');
+var cssnext = require('postcss-cssnext');
+var nested = require('postcss-nested');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 var config = {
   sassPath: './resources/sass',
   bowerDir: './bower_components'
 }
- var processors = [
-    nested,
-    autoprefixer({browsers: ['last 1 version']}),
-    
-  ];
+var processors = [
+  nested,
+  autoprefixer({ browsers: ['last 10 version'] }),
+
+];
 gulp.task('bower', () => {
   return bower()
     .pipe(gulp.dest(config.bowerDir))
@@ -46,24 +46,29 @@ gulp.task('css', () => {
         return 'Error:' + error.message;
       }))
     )
-    
     .pipe(gulp.dest('./public/css'))
-    .pipe(reload({stream:true}))
 });
-gulp.task('watch', () => {
-  gulp.watch(config.sassPath + '/**/*.scss', ['css']);
-  gulp.watch('./public/*.html',reload);
+
+gulp.task('postcss', () => {
+  return gulp.src('./public/css/style.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(reload({ stream: true }))
 })
 
-gulp.task('serve',['bower', 'icons','css', 'watch'],function(){
+gulp.task('watch', () => {
+  gulp.watch(config.sassPath + '/**/*.scss', ['css']);
+  gulp.watch('./public/*.html', reload);
+})
+
+gulp.task('serve', ['bower', 'icons', 'css','postcss', 'watch'], function () {
   browserSync.init({
     open: true,
     port: 8080,
     server: {
       baseDir: "./public",
-      directory:true,
+      directory: true,
     }
-    
   });
 })
 
